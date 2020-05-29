cae1babc4331837c74e28df5a58ecb2d0f24cd1e
xkassa <kassaladislav15@gmail.com>
2020-05-29 11:43:38 +0200

    Verzia 2 pridal sa gyroskop a jeho hodnotz su vypisovane na webstranke, funguje uz aj drive.py, pri odchode zo stranky ma presmeruje na podakovanie

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import time
 import random
 import math
 import smbus
+import os
 import RPi.GPIO as GPIO
-
+import MySQLdb 
+import ConfigParser
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -46,7 +48,47 @@ def distance():
     return distance
 
 
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
 
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+bus = smbus.SMBus(1) 
+address = 0x68     
+bus.write_byte_data(address, power_mgmt_1, 0)
+
+def MPU_9265_getdata():  
+    gxout = read_word_2c(0x43)
+    gyout = read_word_2c(0x45)
+    gzout = read_word_2c(0x47)
+    xout = read_word_2c(0x3b)
+    yout = read_word_2c(0x3d)
+    zout = read_word_2c(0x3f)
+    xout=xout/ 16384.0
+    yout = yout / 16384.0
+    zout = zout / 16384.0
+    return gxout,gyout,gzout,xout,yout,zout
+
+def write2file(Ax,Ay,dist,n):
+    fo = open("static/files/test.txt","a+")    
+    val = '[{"y": 0.6551787400492523, "x": 1, "t": 1522016547.531831}, {"y": 0.47491473008127605, "x": 2, "t": 1522016549.534749}, {"y": 0.7495528524284468, "x": 3, "t": 1522016551.537547}, {"y": 0.19625207463282368, "x": 4, "t": 1522016553.540447}, {"y": 0.3741884249440639, "x": 5, "t": 1522016555.543216}, {"y": 0.06684808042190538, "x": 6, "t": 1522016557.546104}, {"y": 0.17399442194131343, "x": 7, "t": 1522016559.54899}, {"y": 0.025055174467733865, "x": 8, "t": 1522016561.551384}]'
+    fo.write("%s\r\n" %val)
+    return "done"
 
 
 
@@ -59,11 +101,14 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock() 
-
+runvariable='nieco'
+dbV='nieco'
 
 def background_thread(args):
+    dataList = []
     
-    
+    uholx=0
+    uholy=0
     count=0
     countemit=0
     dtime=0.01
@@ -75,19 +120,23 @@ def background_thread(args):
     ka=0.2
     while True:
         socketio.sleep(dtime)
-        count += 1
-        countemit +=1
-        if (countemit == 20):
-            dist=distance();
-            if dist>40:
-                dist=40;
-            socketio.emit('my_response',
-                      {'dist': (round(40-(dist+dist1+dist2)/3)),'count': count},
-                      namespace='/test')  
-            countemit=0;
-            dist2=dist1;
-            dist1=dist;
-            dataCounter = 0 
+        if runvariable=="start":
+            gx,gy,gz,ax,ay,az=MPU_9265_getdata()
+            uholx=ka*(uholx+gy*dtime)+(1-ka)*(math.atan(ay/(math.sqrt(ax*ax+az*az)))*180/math.pi)-5.4
+            uholy=-ka*(uholy+gx*dtime)-(1-ka)*(math.atan(ax/(math.sqrt(ay*ay+az*az)))*180/math.pi)-21.5
+            count += 1
+            countemit +=1
+            if (countemit == 20):
+                dist=distance();
+                if dist>40:
+                    dist=40;
+                socketio.emit('my_response',
+                          {'anglex': round(uholx),'angley': round(uholy),'dist': (round(40-(dist+dist1+dist2)/3)),'gx': gx,'gy': gy,'gz': gz, 'count': count},
+                          namespace='/test')  
+                countemit=0;
+                dist2=dist1;
+                dist1=dist;
+                
 
 @app.route('/', methods=['GET', 'POST'])
 def graphlive():
@@ -96,8 +145,12 @@ def graphlive():
 @socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
     print('Client has Disconnected, ending script')
-    quit()
+    os._exit(0)
     
+
+
+
+
 @socketio.on('connect', namespace='/test')
 def test_connect():
     global thread
@@ -105,6 +158,14 @@ def test_connect():
         if thread is None:
             thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
             print('zaciatok spojenia')
+    
+@socketio.on('db_event', namespace='/test')
+def db_message(message):
+    global dbV
+    dbV = message['value']
+    print(dbV)
+    
+
 
 
 if __name__ == '__main__':
---
+++ drive.py
@@ -0,0 +1,142 @@
+import RPi.GPIO as GPIO
+import time
+import pygame
+import os
+pygame.init()
+j = pygame.joystick.Joystick(0)
+j.init()
+print('Initialized Joystick : %s' % j.get_name())
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+##GPIO_TRIGGER = 24
+##GPIO_ECHO = 21
+##
+##GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+##GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+GPIO.setup(17,GPIO.OUT)
+s=GPIO.PWM(17,50)
+s.start(7)
+
+GPIO.setup(18,GPIO.OUT)
+mf=GPIO.PWM(18,100)
+mf.start(0)
+
+GPIO.setup(23,GPIO.OUT)
+mr=GPIO.PWM(23,100)
+mr.start(0)
+
+GPIO.setup(22,GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(27,GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.output(22, GPIO.HIGH)
+GPIO.output(27, GPIO.HIGH)
+
+anglediff=0
+enginerunsel=False
+enginerunstart=False
+S1=7+anglediff
+MF=0
+MR=0
+Uhol=0
+Rychlost=0
+Treshold=0.2
+TresholdS=0.05
+dist=50
+
+
+def setcar():
+    s.ChangeDutyCycle(S1)
+
+    mf.ChangeDutyCycle(MF)
+    mr.ChangeDutyCycle(MR)
+    
+
+
+
+
+try:   
+    while True:
+        
+        events = pygame.event.get()
+        for event in events:
+          UpdateMotors = 0
+          if event.type==pygame.JOYBUTTONDOWN:
+                if event.button==10:
+                    print('Client has Disconnected, ending script')
+                    GPIO.cleanup()
+                    quit()
+                   
+                    
+                elif event.button==8:
+                    enginerunsel=not enginerunsel
+                    time.sleep(0.3)
+                elif event.button==9:
+                    enginerunstart=not enginerunstart
+                    time.sleep(0.3)
+                elif event.button==5:
+                    if anglediff<0.5:
+                        anglediff=anglediff+0.1
+                        print(anglediff)
+                    time.sleep(0.2)
+                elif event.button==4:
+                    if anglediff>-0.5:
+                        anglediff=anglediff-0.1
+                        print(anglediff)
+                    time.sleep(0.2)
+                
+          # Check if one of the joysticks has moved
+          if event.type == pygame.JOYAXISMOTION:
+            if event.axis == 1:
+              Rychlost = event.value
+              UpdateMotors = 1
+            elif event.axis == 3:
+              Uhol = event.value
+              UpdateMotors = 1
+              
+            if UpdateMotors:
+                aUhol=abs(Uhol)
+                if (aUhol > TresholdS):
+                  S1=round(Uhol*1.5+7+anglediff,2)
+                  if S1>8.5:
+                      S1=8.5
+                  if S1<5.5:
+                      S1=5.5
+                  
+                    
+                else: S1=7+anglediff
+                
+                if (Rychlost < -Treshold):
+                  MF=-Rychlost*90
+                else: MF=0
+                
+                if (Rychlost > Treshold):
+                  MR=Rychlost*50
+                else:
+                  MR=0
+                  
+                
+                if (MR>0 and MF>0):
+                    MR=0
+                    MF=0
+                
+                if (enginerunsel and enginerunstart):
+                    setcar()
+                    
+                    
+                else:
+                    print(enginerunsel)
+                    print(enginerunstart)
+                    MR=0
+                    MF=0
+                    S1=7+anglediff
+                    setcar()
+        
+    time.sleep(0.2)
+        
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
